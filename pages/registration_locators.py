84cfc13c815837df5cf1ca92727cdecb089135c3
Rillplay1 <voda66622@gmail.com>
2025-04-30 20:13:56 +0300

    Автотесты для рег и авто

---
+++ pages/registration_locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+class RegistrationLocators:
+    REGISTER_BTN3 = (By.XPATH, '(//a[@href="/registration"])[4]')
+    REGISTER_BTN4 = (By.XPATH, '(//a[@href="/registration"])[5]')
